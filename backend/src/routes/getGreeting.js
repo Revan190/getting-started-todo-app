066e19db48f8cb5451ff4f01b832983622a4283b
Revan190 <programmer23w@gmail.com>
2024-07-07 13:49:22 +0200

    Just edit

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -3,6 +3,7 @@ const GREETINGS = [
     "All hands on deck!",
     "Charting the course ahead!",
 ];
+
 module.exports = async (req, res) => {
     res.send({
         greeting: GREETINGS[ Math.floor( Math.random() * GREETINGS.length )],
